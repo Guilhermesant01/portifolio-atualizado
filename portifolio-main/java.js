736d8b82886732fcfe6c4c2c58f105cd0c805359
Guilhermesant01 <guilhermesalomao2005@gmail.com>
2024-12-12 08:16:20 -0300

    Add files via upload

---
+++ portifolio-main/java.js
@@ -0,0 +1,7 @@
+document.getElementById('Afonte').addEventListener('click', function () {
+    document.body.style.fontSize = '18px';
+});
+
+document.getElementById('Dfonte').addEventListener('click', function () {
+    document.body.style.fontSize = '14px';
+});
